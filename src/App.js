4483dab80acdf6662b251c60f70cd4cfd27f41a7
Julian Barrios <julianbarrios0821@gmail.com>
2021-09-08 21:33:56 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            VinylShop
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Card, CardMedia, CardContent, CardActions, Typography, IconBotton} from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+import { mergeClasses } from '@material-ui/styles';
+
+const Product = () => {
+    return (
+        <Card className={classes.root}>
+            <CardMedia/>
+        </Card>
+    )
+}
+
+export default Product
